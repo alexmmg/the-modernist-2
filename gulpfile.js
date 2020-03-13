8c405543d24e1a46d1768d7a8d9779107ba258cd
Den Skorov <den.scorov1@gmail.com>
2020-03-13 13:19:25 +0200

    add a bit new gulp feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-
-
 const browserSync = require('browser-sync');
 
+const path = {
+    scss: ''
+};
+
+
 function style() {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src(path.scss)
         .pipe(sass())
         .pipe(gulp.dest('./css'))
 }
@@ -14,5 +17,12 @@ function watch() {
     gulp.watch('./scss/**/*.scss', style);
 }
 
+function defaultTask(cb){
+cb();
+}
+
 exports.style = style;
 exports.watch = watch;
+
+
+exports.default = defaultTask;
