bc453686a489a087e8450c74790dae67cea11427
alexmmg <mmg.prokazyuk.o@gmail.com>
2020-03-13 16:43:25 +0200

    friday commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,26 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 
-const path = {
-    scss: ''
-};
+// const path = {
+//     scss: './scss/style.scss',
+//     css: './app/css/style.css'
+// };
 
 
 function style() {
-    return gulp.src(path.scss)
+    return gulp.src('./scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./app/css/'))
 }
 
 function watch() {
     gulp.watch('./scss/**/*.scss', style);
 }
 
-function defaultTask(cb){
-cb();
-}
+// function defaultTask(cb) {
+//     cb();
+// }
 
 exports.style = style;
 exports.watch = watch;
-
-
-exports.default = defaultTask;
+// exports.default = defaultTask;
