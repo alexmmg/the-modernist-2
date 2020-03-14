cd263a20d7774c016e0b08e42c80dafedd625db1
alexmmg <mmg.prokazyuk.o@gmail.com>
2020-03-14 22:49:57 +0200

    check finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// let ready = $(document).ready(function () {
-//     $('.owl-carousel').owlCarousel({
-//         loop: true,
-//         margin: 10,
-//         nav: true,
-//         navText: ["<img alt='Left' src='../img/section-1/Left-arrow.png'>", "<img alt='Right' src='../img/section-1/Right-arrow.png'>"],
-//         items: 1,
-//     })
-// });
 
 
 
